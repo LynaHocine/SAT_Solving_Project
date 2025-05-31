7733d61a025316b1319b23a890f9a8fe1645e563
LynaHocine <lyna.hocine@etu.sorbonne-universite.fr>
2025-05-31 11:55:10 +0200

    Implementing a non-recursive SAT Solver with BCP and watched literals

--- non_rec_bcp_watched.py
+++ non_rec_bcp_watched.py
@@ -1,5 +1,4 @@
 
-
 from collections import defaultdict
 from parser_dimacs import parse_dimacs
 
@@ -12,6 +11,7 @@ def bcp(assignment, watched, watch_list,formula, newly_assigned_literals):
         lit = propagation_queue.pop()
         neg_lit = -lit
         clauses_to_check = watch_list[neg_lit].copy()
+
         for id in clauses_to_check:
             clause = formula[id]
             w1, w2 = watched[id]
@@ -22,7 +22,7 @@ def bcp(assignment, watched, watch_list,formula, newly_assigned_literals):
                 if l!= other and (l not in assignment or assignment[l] is True):
                     watched[id] = (other, l)
                     watch_list[l].add(id)
-                    watch_list[neg_lit].remove[id]
+                    watch_list[neg_lit].remove(id)
                     found_new = True
                     break
             
@@ -36,7 +36,13 @@ def bcp(assignment, watched, watch_list,formula, newly_assigned_literals):
 
     return True
 
-    
+def all_vars_assigned(assignment, all_literals):
+    return all(var in assignment or -var in assignment for var in all_literals)
+
+def select_unassigned(assignment, all_literals):
+    for var in all_literals:
+        if var not in assignment and -var not in assignment:
+            return var
 
 def non_rec_dpll(filename):
     formula = parse_dimacs(filename)
@@ -48,6 +54,7 @@ def non_rec_dpll(filename):
     watch_list = defaultdict(set)
     watched ={}
     
+    unit_literals = []
     #initializing watched literals per clause
     for id, clause in enumerate(formula):
         if(len(clause) >= 2):
@@ -57,7 +64,33 @@ def non_rec_dpll(filename):
             watch_list[lit2].add(id)
         elif (len(clause) == 1):
             lit = clause[0]
-            watched[id]=(lit,)
+            watched[id]=(lit,lit)
             watch_list[lit].add(id)
+            unit_literals.append(lit)
         else:
             watched[id]=()
+
+
+    if not bcp(assignment, watched, watch_list, formula, unit_literals):
+        return False
+    
+    while True:
+        success = bcp(assignment, watched, watch_list, formula, newly_assigned_literals=[])
+        if not success:
+            if not stack:
+                return False
+            lit, prev_assignment = stack.pop()
+            assignment=prev_assignment.copy()
+            assignment[-lit]=True
+            if not bcp(assignment, watched, watch_list, formula, [-lit]):
+                continue
+        else:
+            if all_vars_assigned(assignment,all_literals):
+                return assignment
+            
+            unassigned = select_unassigned(assignment, all_literals)
+            stack.append((unassigned, assignment.copy()))
+            assignment[unassigned]= True
+
+    return False
+
